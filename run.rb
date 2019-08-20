5ac6acf9e95710e629578d02380abc84b586d98c
Mesut Güneş <gunesmes@gmail.com>
2019-08-20 15:48:12 +0300

    adding run.rb info

--- run.rb
+++ run.rb
@@ -5,9 +5,9 @@ target = ARGV.shift
 ENV['PLATFORM'] = target
 
 if target == 'android'
-  exec("rspec ./src/specs/home_page_spec.rb #{ARGV.join(' ')}")
+  exec("rspec ./src/specs #{ARGV.join(' ')}")
 elsif target == 'ios'
-  exec("rspec ./src/specs/home_page_spec.rb #{ARGV.join(' ')}")
+  exec("rspec ./src/specs #{ARGV.join(' ')}")
 else
   puts "- Invalid target: #{target}"
   puts "- Available targets are: \n  * ios\n  * android\n"
